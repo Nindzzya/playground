eef6e075d3dc3c54ee8dc2e23ea659e61aac6b05
Kesava Prasad Arul <kesavaprasadarul@outlook.com>
2016-10-10 12:43:00 +0530

    Inital code

---
+++ BitmapInvert/BitmapInvert/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitmapInvert
+{
+    public partial class Form1 : Form
+    {
+        byte[] imageByte;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "bmp files (*.bmp)|*.bmp";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap x = new Bitmap(dlg.FileName);
+                    imageByte = imageToByteArray(x); //this is the byte array that you need to manipulate.
+                    pictureBox1.Image = new Bitmap(dlg.FileName);
+                }
+            }
+        }
+        public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            return ms.ToArray();
+        }
+
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
