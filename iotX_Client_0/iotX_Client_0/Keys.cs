787688d66540687a8ea32f08c8aa9495b3cd93c0
Kesava Prasad Arul <kesavaprasadarul@outlook.com>
2016-11-25 21:16:33 +0530

    All changes in iotClient -Copied many recent sources -Made init, won't work

---
+++ iotX_Client_0/iotX_Client_0/Keys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iotX_Backend_Test
+{
+    public class Keys
+    {
+        public static int appId = 48524;
+        public static string authKey = "qnMaLbHTbCqTaVK";
+        public static string authSecret = "ZeFx38MG9ydmPym";
+        public static string apiEndpoint = "https://api.quickblox.com/";
+        public static string chatEndpoint = "chat.quickblox.com";
+    }
+}
