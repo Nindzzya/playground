eaf1860493243458c26f159ae89583af5fe33e6b
Kesava Prasad Arul <kesavaprasadarul@outlook.com>
2017-02-12 18:20:14 +0530

    -Build success -Revised Grammar (shortened) -Continuous Recognition Enabled -Available Commands are Turn on/off/Put on/off fan/light -Try Bug: Requires Internet

--- iotX_Client_0/iotX_Client_0/MainPage.xaml.cs
+++ iotX_Client_0/iotX_Client_0/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Security.ExchangeActiveSyncProvisioning;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -28,7 +29,7 @@ namespace iotX_Client_0
         {
             this.InitializeComponent();
             initX();
-            initSpeech();
+           initSpeech();
         }
 
         public async void initX()
@@ -48,9 +49,46 @@ namespace iotX_Client_0
         public async void initSpeech()
         {
             var speechRecognizer = new Windows.Media.SpeechRecognition.SpeechRecognizer();
-            var storageFile = await Windows.Storage.StorageFile.GetFileFromApplicationUriAsync(new Uri(@"/SRGS-list.grxml",UriKind.Relative));
-            var grammarFileConstraint = new Windows.Media.SpeechRecognition.SpeechRecognitionGrammarFileConstraint(storageFile, "colors");
+            var url = new Uri("ms-appx:///SRGS-list.grxml").ToString();
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(url));
+            var grammarFileConstraint = new Windows.Media.SpeechRecognition.SpeechRecognitionGrammarFileConstraint(file);
+            speechRecognizer.UIOptions.ExampleText = @"Ex. 'blue background', 'green text'";
+            speechRecognizer.Constraints.Add(grammarFileConstraint);
 
+            // Compile the constraint.
+            var status = await speechRecognizer.CompileConstraintsAsync();
+            speechRecognizer.ContinuousRecognitionSession.Completed += ContinuousRecognitionSession_Completed;
+            speechRecognizer.ContinuousRecognitionSession.ResultGenerated += ContinuousRecognitionSession_ResultGenerated;
+            await speechRecognizer.ContinuousRecognitionSession.StartAsync();
+            // Start recognition.
+            //var speechRecognitionResult = await speechRecognizer.RecognizeWithUIAsync();
+
+            // Do something with the recognition result.
+            //var messageDialog = new Windows.UI.Popups.MessageDialog(speechRecognitionResult.Text, "Text spoken");
+           // await messageDialog.ShowAsync();
+
+        }
+
+        private async void ContinuousRecognitionSession_ResultGenerated(Windows.Media.SpeechRecognition.SpeechContinuousRecognitionSession sender, Windows.Media.SpeechRecognition.SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        {
+            var sResult = args.Result;
+            if (sResult.SemanticInterpretation.Properties.ContainsKey("TURNON")&&sResult.SemanticInterpretation.Properties["TURNON"][0].ToString() != "...")
+            {
+                string objectX = sResult.SemanticInterpretation.Properties["TURNON"][0].ToString();
+                var messageDialog = new Windows.UI.Popups.MessageDialog(objectX + " is turned on.", "Text spoken");
+                await messageDialog.ShowAsync();
+            }
+            if (sResult.SemanticInterpretation.Properties.ContainsKey("TURNOFF") && sResult.SemanticInterpretation.Properties["TURNOFF"][0].ToString() != "...")
+            {
+                string objectX = sResult.SemanticInterpretation.Properties["TURNOFF"][0].ToString();
+                var messageDialog = new Windows.UI.Popups.MessageDialog(objectX + " is turned off.", "Text spoken");
+                await messageDialog.ShowAsync();
+            }
+        }
+
+        private void ContinuousRecognitionSession_Completed(Windows.Media.SpeechRecognition.SpeechContinuousRecognitionSession sender, Windows.Media.SpeechRecognition.SpeechContinuousRecognitionCompletedEventArgs args)
+        {
+            throw new NotImplementedException();
         }
     }
 }
